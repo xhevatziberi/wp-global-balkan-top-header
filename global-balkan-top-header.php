9353efe6c3d93c689655ac4d9230411215d5522d
xhevattalenta <xhevat@talenta.mk>
2020-03-27 16:12:06 +0100

    first 11

--- global-balkan-top-header.php
+++ global-balkan-top-header.php
@@ -21,23 +21,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-include_once('updater.php');
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-	$config = array(
-		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-		'proper_folder_name' => 'wp-global-balkan-top-header-master', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/xhevatziberi/wp-global-balkan-top-header', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.githubusercontent.com/xhevatziberi/wp-global-balkan-top-header/master', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header/zipball/master', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-		'requires' => '3.0', // which version of WordPress does your plugin require?
-		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-		'readme' => 'README.md', // which file to use as the readme for the version number
-		'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-	);
-	new WP_GitHub_Updater($config);
-}
 
 // Load plugin class files.
 require_once 'includes/class-global-balkan-top-header.php';
@@ -109,4 +92,34 @@ function add_header() {
   // Echo the html
   echo "<div>Special offer! June only: Free chocolate for everyone!</div>";
 	include_once( 'template.php' );
+}
+
+/*updater*/
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'wp-global-balkan-top-header-master',
+			'api_url' => 'https://api.github.com/repos/xhevatziberi/wp-global-balkan-top-header',
+			'raw_url' => 'https://raw.github.com/xhevatziberi/wp-global-balkan-top-header/master',
+			'github_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header',
+			'zip_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+
 }
