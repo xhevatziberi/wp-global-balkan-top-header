6e1ae17cc19c5f44a0fd4417a979ff8029cc1d57
xhevattalenta <xhevat@talenta.mk>
2020-03-27 16:51:17 +0100

    first 13

--- global-balkan-top-header.php
+++ global-balkan-top-header.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Global Balkan Top Header
- * Version: 1.0.4
+ * Version: 1.0.5
  * Plugin URI: http://www.hughlashbrooke.com/
  * Description: This is your starter template for your next WordPress plugin.
  * Author: Hugh Lashbrooke
