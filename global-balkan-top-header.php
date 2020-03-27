411261dfd213bfbe8c5ee77ef84948e624344cd7
xhevattalenta <xhevat@talenta.mk>
2020-03-27 15:24:18 +0100

    first 2

--- global-balkan-top-header.php
+++ global-balkan-top-header.php
@@ -26,10 +26,10 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 	$config = array(
 		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 		'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/username/repository-name', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/username/repository-name/master', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/username/repository-name', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the GitHub repo
+		'api_url' => 'https://api.github.com/repos/xhevatziberi/wp-global-balkan-top-header', // the GitHub API url of your GitHub repo
+		'raw_url' => 'https://raw.github.com/xhevatziberi/wp-global-balkan-top-header/master', // the GitHub raw url of your GitHub repo
+		'github_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header', // the GitHub url of your GitHub repo
+		'zip_url' => 'https://github.com/xhevatziberi/wp-global-balkan-top-header/zipball/master', // the zip url of the GitHub repo
 		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 		'requires' => '3.0', // which version of WordPress does your plugin require?
 		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
