a7a00fe24e6ca1b1d8eeb82962261a075b013236
Xhevat Ziberi <xhevat.ziberi@gmail.com>
2020-03-29 22:55:48 +0200

    cahnges

--- template3.php
+++ template3.php
@@ -2,16 +2,16 @@
     <div class="topbar-content bootstrap-wrapper" style="height: 0px;">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 col-md-5">
+                <div class="vc_col-sm-12 vc_col-md-5">
                   <h3>GLOBAL BALKAN</h3>
                   <p>Global Ballkan is a company that brings you quality and correctness. Spread across several sectors, Global Balkan enables you to easily make your choice in construction, central heating and ceramic tiles.</p>
                 </div>
-                <div class="col-xs-12 col-sm-2 col-md-2">
+                <div class="vc_col-xs-12 vc_col-sm-2 vc_col-md-2">
                     <div class="choose-country">
                         <p><i class="icon-world-map"></i></p>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-10 col-md-5">
+                <div class="vc_col-xs-12 vc_col-sm-10 vc_col-md-5">
                     <div class="activities">
                         <ul class="activities-icons">
                             <li class="dropdown open" style="width: 33%;">
@@ -35,22 +35,22 @@
                         </ul>
                         <div class="activities-list">
                           <div class="dropdown-menu  open" role="menu" id="industryId-1">
-                            <ul class="col-xs-6 col-sm-4 col-md-4">
-                              <li class="col-md-12">
+                            <ul class="vc_col-xs-6 vc_col-sm-4 vc_col-md-4">
+                              <li class="vc_col-md-12">
                                 <a href="http://screenmedia.mk/">
                                   <img src="<?php echo esc_url($instance->assets_url) . 'img/screenmedia-icon.png' ?>" alt="screenmedia" title="screenmedia">
                                 </a>
                               </li>
                             </ul>
-                            <ul class="col-xs-6 col-sm-4 col-md-4">
-            									<li class="col-md-12">
+                            <ul class="vc_col-xs-6 vc_col-sm-4 vc_col-md-4">
+            									<li class="vc_col-md-12">
             										<a href="http://talenta.mk/">
             											<img src="<?php echo esc_url($instance->assets_url) . 'img/talenta-icon.png' ?>" alt="talenta" title="talenta">
             										</a>
             									</li>
                             </ul>
-                            <ul class="col-xs-6 col-sm-4 col-md-4">
-                              <li class="col-md-12">
+                            <ul class="vc_col-xs-6 vc_col-sm-4 vc_col-md-4">
+                              <li class="vc_col-md-12">
                                 <a href="#">
                                   <img src="<?php echo esc_url($instance->assets_url) . 'img/balkan-icon.png' ?>" alt="balkan" title="balkan">
                                 </a>
@@ -58,8 +58,8 @@
                             </ul>
                           </div>
                           <div class="dropdown-menu " role="menu" id="industryId-2">
-                              <ul class="col-xs-6 col-sm-4 col-md-4">
-                								<li class="col-md-12">
+                              <ul class="vc_col-xs-6 vc_col-sm-4 vc_col-md-4">
+                								<li class="vc_col-md-12">
                 									<a href="http://eurovia.mk/">
                 										<img src="<?php echo esc_url($instance->assets_url) . 'img/eurovia-icon.png' ?>" alt="eurovia" title="eurovia">
                 									</a>
@@ -67,8 +67,8 @@
                               </ul>
                           </div>
             							<div class="dropdown-menu " role="menu" id="industryId-3">
-                            <ul class="col-xs-6 col-sm-4 col-md-4">
-            									<li class="col-md-12">
+                            <ul class="vc_col-xs-6 vc_col-sm-4 vc_col-md-4">
+            									<li class="vc_col-md-12">
             										<a href="http://enea.mk/">
             											<img src="<?php echo esc_url($instance->assets_url) . 'img/enea-icon.png' ?>" alt="enea" title="enea">
             										</a>
@@ -85,11 +85,11 @@
     <div class="global-nav bootstrap-wrapper">
         <div class="container">
             <div class="row">
-                <div class="col-sm-2 col-md-2">
+                <div class="vc_vc_col-sm-2 vc_vc_col-md-2">
                     <button class="btn-link" id="topbar-toggle" type="button">GLOBAL BALKAN&nbsp;<span class="caret"></span>
                     </button>
                 </div>
-                <div class="col-sm-10 col-md-10">
+                <div class="vc_vc_col-sm-10 vc_vc_col-md-10">
                     <ul class="global-menu-controls">
                         <li class="dropdown dropdown_header-search">
                             <a href="javascript:void(0);" class="dropdown-toggle dropdown-toggle_search-btn"><i class="fa fa-search"></i></a>
@@ -99,12 +99,7 @@
                                     <input type="text" value="" name="s" id="top-area-s" class="form-control inputSearchQuery" placeholder="Search">
                                     <input type="hidden" name="q" class="searchQuery">
                                     <span class="input-group-btn">
-                											<!--<button id="top-area-searchsubmit" class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>-->
-                                      <button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-                      									<span class="toggle-inner">
-                      										<svg class="svg-icon" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23"><path d="M38.710696,48.0601792 L43,52.3494831 L41.3494831,54 L37.0601792,49.710696 C35.2632422,51.1481185 32.9839107,52.0076499 30.5038249,52.0076499 C24.7027226,52.0076499 20,47.3049272 20,41.5038249 C20,35.7027226 24.7027226,31 30.5038249,31 C36.3049272,31 41.0076499,35.7027226 41.0076499,41.5038249 C41.0076499,43.9839107 40.1481185,46.2632422 38.710696,48.0601792 Z M36.3875844,47.1716785 C37.8030221,45.7026647 38.6734666,43.7048964 38.6734666,41.5038249 C38.6734666,36.9918565 35.0157934,33.3341833 30.5038249,33.3341833 C25.9918565,33.3341833 22.3341833,36.9918565 22.3341833,41.5038249 C22.3341833,46.0157934 25.9918565,49.6734666 30.5038249,49.6734666 C32.7048964,49.6734666 34.7026647,48.8030221 36.1716785,47.3875844 C36.2023931,47.347638 36.2360451,47.3092237 36.2726343,47.2726343 C36.3092237,47.2360451 36.347638,47.2023931 36.3875844,47.1716785 Z" transform="translate(-20 -31)"></path></svg>										<span class="toggle-text">Search</span>
-                      									</span>
-                      								</button>
+                											<button id="top-area-searchsubmit" class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
                 										</span>
                                   </form>
                                 </li>
