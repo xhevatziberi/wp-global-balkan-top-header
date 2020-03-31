608967c2b17aeae9fbe568bb8026f9b00d8f7e90
xhevattalenta <xhevat@talenta.mk>
2020-03-31 13:07:55 +0200

    fixed design and other stuff

--- template-withdropdown.php
+++ template-withdropdown.php
@@ -2,8 +2,8 @@
     <div class="topbar-content bootstrap-wrapper" style="height: 0px;">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 col-md-5 about">
-                  <h3><a href="http://globalbalkan.mk/">GLOBAL BALKAN</a></h3>
+                <div class="col-sm-12 col-md-5">
+                  <h3> GLOBAL BALKAN</h3>
                   <p><?php _e('Global Ballkan is a company that brings you quality and correctness. Spread across several sectors, Global Balkan enables you to easily make your choice in construction, central heating and ceramic tiles.', 'global-balkan-top-header') ?></p>
                 </div>
                 <div class="col-12 col-sm-2 col-md-2">
@@ -12,34 +12,56 @@
                     </div>
                 </div>
                 <div class="col-12 col-sm-10 col-md-5">
-                    <div class="row activities">
-                      <div class="col-4 text-center">
-                        <a class="sector" href="http://estate.globalbalkan.mk/">
-                          <i class="icon-activities-construction"></i>
-                          <span class="industry second hidden-phone hidden-tablet"><?php _e('Construction', 'global-balkan-top-header') ?></span>
-                        </a>
-                        <a class="company" href="http://estate.globalbalkan.mk/">
-                          <img src="<?php echo esc_url($instance->assets_url) . 'img/global_estate.png' ?>" alt="global_estate" title="global_estate">
-                        </a>
-                      </div>
-                      <div class="col-4 text-center">
-                        <a class="sector" href="http://ceramic.globalbalkan.mk/">
-                          <i class="icon-activities-ceramic"></i>
-                          <span class="industry second hidden-phone hidden-tablet"><?php _e('Ceramic', 'global-balkan-top-header') ?></span>
-                        </a>
-                        <a class="company" href="http://ceramic.globalbalkan.mk/">
-                          <img src="<?php echo esc_url($instance->assets_url) . 'img/global_ceramic.png' ?>" alt="global_ceramic" title="global_ceramic">
-                        </a>
-                      </div>
-                      <div class="col-4 text-center">
-                        <a class="sector" href="http://term.globalbalkan.mk/">
-                          <i class="icon-activities-hvac"></i>
-                          <span class="industry second hidden-phone hidden-tablet"><?php _e('HVAC', 'global-balkan-top-header') ?></span>
-                        </a>
-                        <a class="company" href="http://term.globalbalkan.mk/">
-                          <img src="<?php echo esc_url($instance->assets_url) . 'img/global_term.png' ?>" alt="global_term" title="global_term">
-                        </a>
-                      </div>
+                    <div class="activities">
+                        <ul class="activities-icons">
+                            <li class="dropdown open" style="width: 33%;">
+                                <a class="dropdown-toggler" href="#industryId-1">
+                                  <i class="icon-activities-construction"></i>
+                                  <span class="industry second hidden-phone"><?php _e('Construction', 'global-balkan-top-header') ?></span>
+                                </a>
+                            </li>
+                            <li class="dropdown" style="width: 33%;">
+                                <a class="dropdown-toggler" href="#industryId-2">
+                                  <i class="icon-activities-ceramic"></i>
+                                  <span class="industry second hidden-phone"><?php _e('Ceramic', 'global-balkan-top-header') ?></span>
+                                </a>
+                            </li>
+              							<li class="dropdown" style="width: 33%;">
+                              <a class="dropdown-toggler" href="#industryId-3">
+                                <i class="icon-activities-hvac"></i>
+              									<span class="industry second hidden-phone"><?php _e('HVAC', 'global-balkan-top-header') ?></span>
+              								</a>
+                            </li>
+                        </ul>
+                        <div class="activities-list">
+                          <div class="dropdown-menu  open" role="menu" id="industryId-1">
+                            <ul class="col-6 col-sm-6 col-md-6">
+                              <li class="col-md-12">
+                                <a href="http://estate.globalbalkan.mk/">
+            											<img src="<?php echo esc_url($instance->assets_url) . 'img/global_estate.png' ?>" alt="global_estate" title="global_estate">
+            										</a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div class="dropdown-menu " role="menu" id="industryId-2">
+                              <ul class="col-6 col-sm-6 col-md-6">
+                								<li class="col-md-12">
+                									<a href="http://ceramic.globalbalkan.mk/">
+                										<img src="<?php echo esc_url($instance->assets_url) . 'img/global_ceramic.png' ?>" alt="global_ceramic" title="global_ceramic">
+                									</a>
+                								</li>
+                              </ul>
+                          </div>
+            							<div class="dropdown-menu " role="menu" id="industryId-3">
+                            <ul class="col-6 col-sm-6 col-md-6">
+            									<li class="col-md-12">
+                                <a href="http://term.globalbalkan.mk/">
+                                  <img src="<?php echo esc_url($instance->assets_url) . 'img/global_term.png' ?>" alt="global_term" title="global_term">
+                                </a>
+            									</li>
+                            </ul>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -76,7 +98,7 @@
                     </ul>
                     <ul class="global-menu hidden-phone">
                         <li class="choose-country">
-                            <a href="http://globalbalkan.mk/"><i class="icon-world-map"></i></a>
+                            <a href="#!"><i class="icon-world-map"></i></a>
                         </li>
                         <li>
                           <a href="http://globalbalkan.mk/sectors/"><?php _e('Sectors', 'global-balkan-top-header') ?></a>
@@ -89,24 +111,6 @@
                         </li>
                     </ul>
                 </div>
-            </div>
-            <div class="row hidden-tablet hidden-desktop">
-              <div class="col-12">
-                <ul class="global-menu">
-                    <li class="choose-country">
-                        <a href="http://globalbalkan.mk/"><i class="icon-world-map"></i></a>
-                    </li>
-                    <li>
-                      <a href="http://globalbalkan.mk/sectors/"><?php _e('Sectors', 'global-balkan-top-header') ?></a>
-                    </li>
-                    <li>
-                      <a href="http://globalbalkan.mk/careers/"><?php _e('Career', 'global-balkan-top-header') ?></a>
-                    </li>
-                    <li>
-                      <a href="http://globalbalkan.mk/contact/"><?php _e('Contacts', 'global-balkan-top-header') ?></a>
-                    </li>
-                </ul>
-              </div>
             </div>
         </div>
     </div>
